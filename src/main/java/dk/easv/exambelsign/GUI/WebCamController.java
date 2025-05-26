69dde8ad9d8dd1c9422f18b3ac026f56cd47fe65
Victor Andersen <vicand01@easv365.dk>
2025-05-26 09:19:27 +0200

    scenebuilder changes to fxml

--- src/main/java/dk/easv/exambelsign/GUI/WebCamController.java
+++ src/main/java/dk/easv/exambelsign/GUI/WebCamController.java
@@ -1,10 +1,19 @@
 package dk.easv.exambelsign.GUI;
 
-import javafx.scene.Camera;
+
+import javafx.scene.media.Media;
+
+import java.awt.*;
 
 public class WebCamController {
 
+    // Need live video feed, able to be snapshot and return a png to be saved.
+
+    
 
 
-}
+
+
+
+ }
 
