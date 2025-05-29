661d6c753e3b84e39c429d317e3b61e01cee0eb8
FungusChungus <vicand01@easv365.dk>
2025-05-29 11:16:53 +0200

    fxml changes

---
+++ src/main/java/dk/easv/exambelsign/GUI/PhotosController.java
@@ -0,0 +1,15 @@
+package dk.easv.exambelsign.GUI;
+
+import javafx.scene.image.ImageView;
+
+public class PhotosController {
+
+
+    public ImageView FirstImage;
+    public ImageView SecondImage;
+    public ImageView ThirdImage;
+    public ImageView FourthImage;
+    public ImageView FifthImage;
+    public ImageView SixthImage;
+
+}
